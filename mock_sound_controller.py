cc09522748aee5640a3deb3900a6bef53007e29f
SangHun <myks790@gmail.com>
2018-05-01 08:42:01 +0000

    add mock_sound_controller

---
+++ mock_sound_controller.py
@@ -0,0 +1,4 @@
+import pygame
+
+def play_sound(path):
+    print("play",path)
--- test_sound_controller.py
+++ test_sound_controller.py
@@ -2,5 +2,9 @@ import sound_controller
 import time
 
 sound_controller.play_sound("sounds/test_sound.wav")
-
 time.sleep(2)
+
+import mock_sound_controller
+mock_sound_controller.play_sound("sounds/test_sound.wav")
+
+
