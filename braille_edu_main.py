2d58e92e1b4a79842a30356b85093ecbed37cf54
myks790 <myks790@gmail.com>
2018-05-22 21:33:19 +0900

    기존 코드 정리

--- braille_edu_main.py
+++ braille_edu_main.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
-
-# import RPi.GPIO as GPIO
-import time
-# import pyttsx3
-import random
-# import pygame
 from mock_braille_controller import SolenoidController
+from mock_braille_controller import ButtonListener
+from mock_braille_controller import AnswerReader
+import mock_sound_controller
+import random
+import enum
 
 # 전역변수
 play_mode = 0  # 교육:1 / 문제:2
@@ -21,131 +20,6 @@ hangle_middle = [[0, 2, 5], [1, 3, 4], [1, 2, 4], [0, 3, 5], [0, 4, 5], [1, 4, 5
                  [0, 3, 4], [0, 2, 3, 4], [0, 1, 3, 4]]  # ㅏ,ㅑ,ㅓ,ㅕ,ㅗ,ㅛ,ㅜ,ㅠ,ㅡ,ㅣ,ㅐ,ㅔ
 hangle_end = [[0], [2, 3], [3, 4], [2], [2, 5], [0, 2], [4], [2, 3, 4, 5], [0, 4], [2, 4], [2, 3, 4], [2, 4, 5],
               [2, 3, 5], [3, 4, 5]]  # ㄱ~ㅎ
-sript = ['Select Play mode', 'Select Play language']
-
-
-def main():
-    global play_mode
-    global language_selection
-    talker('Select play mode')
-    play_mode = input("Select Play mode (edu:0 or quize:1) : ")  # button 실행모드확인
-    talker('Select Play language')
-    language_selection = input("Select Play language (english:0 or hangle:1) : ")  # button 언어확인
-
-    # 1 교육모드
-    if play_mode == 0:
-        print('Start Edu!')
-        talker('Start edu')
-
-        if language_selection == 0:  # 영어 시작
-            print('English start')
-            # english_edu()
-        else:  # 한글 시작
-            print('hangle start')
-            hangle_edu()
-
-    # 2 문제모드
-    else:
-        print('Start Quize!')
-        talker('Start quize')
-
-        if language_selection == 0:  # 영어 시작
-            print('English start')
-            # english_quize()
-        else:  # 한글 시작
-            print('hangle start')
-            hangle_quize()
-
-
-def talker(string):
-    time.sleep(2)
-    # engine = pyttsx3.init() # tts init
-    # engine.setProperty('rate', 100)
-    # engine.say(string)
-    # engine.runAndWait()
-
-
-def hangle_edu():
-    solenoid = SolenoidController()
-
-    # 버튼 출력(초성)
-    for i in range(len(hangle_front)):
-        button_up = hangle_front[i]
-        for j in range(len(button_up)):
-            solenoid.on(button_up[j])
-
-        # 스피커 출력
-        mock_sound_controller.play_sound()
-        time.sleep(2.0)
-
-        # 버튼확인 및 버튼 초기화
-        talker('Solenoid check')
-        print('Solenoid check')
-        pause = input()  # 확인버튼값을 받아야함
-
-        if pause == True:
-            print('Next dictionary')
-
-    # 버튼 출력(중성)
-    for i in range(len(hangle_middle)):
-        button_up = hangle_middle[i]
-        for j in range(len(button_up)):
-            solenoid.on(button_up[j])
-
-        # 스피커 출력
-        mock_sound_controller.play_sound()
-        time.sleep(2.0)
-
-        # 버튼확인 및 버튼 초기화
-        talker('Solenoid check')
-        print('Solenoid check')
-        pause = input()  # 확인버튼값을 받아야함
-
-        if pause == True:
-            print('Next dictionary')
-
-    # 버튼 출력(종성)
-    for i in range(len(hangle_end)):
-        button_up = hangle_end[i]
-        for j in range(len(button_up)):
-            solenoid.on(button_up[j])
-
-        # 스피커 출력
-        mock_sound_controller.play_sound()
-        time.sleep(2.0)
-
-        # 버튼확인 및 버튼 초기화
-        talker('Solenoid check')
-        print('Solenoid check')
-        pause = input()  # 확인버튼값을 받아야함
-
-        if pause == True:
-            print('Next dictionary')
-
-
-def hangle_quize():
-    for i in range(len(english)):
-        problem = english[random.randint(1, 26)]  # 문제
-        #
-        #     # 스피커 출력
-        mock_sound_controller.play_sound()
-    #     time.sleep(2.0)
-    #
-    #     answer = input() # 정답입력 받고
-    #
-    # if  == :  #정답확인
-    #     talker('success')
-    #     print('success')
-    #
-    # else:
-    #     talker('fail')
-    #     print('fail')
-
-
-from mock_braille_controller import ButtonListener
-from mock_braille_controller import AnswerReader
-import mock_sound_controller
-import enum
 
 
 class Language(enum.Enum):
