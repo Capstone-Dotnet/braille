07726de5cad4be3c6c9c0fd3ba868c3fcd677b02
myks790 <myks790@gmail.com>
2018-05-26 12:46:37 +0900

    Sound Conroller 버그 수정3

--- sound_controller.py
+++ sound_controller.py
@@ -21,23 +21,23 @@ class SoundController:
             self._prefix_path = "sound/영어/"
 
     def say_answer_success(self):
-        self.play_sound("정답입니다.")
+        print("정답입니다.")
 
     def say_answer_fail(self):
-        self.play_sound("오답입니다.")
+        print("오답입니다.")
 
     def say_selected_language(self):
-        self.play_sound("한국어입니다.")
+        print("한국어입니다.")
 
     def say_selected_mode(self, mode):
         if mode == GameMode.QUIZ:
-            self.play_sound("퀴즈모드입니다.")
+            print("퀴즈모드입니다.")
         elif mode == GameMode.EDUCATION:
-            self.play_sound("문제모드입니다.")
+            print("문제모드입니다.")
 
-    def play_sound(self, path):
-        self.mixer.Sound(path).play()
-        print("play sound : ", path)
+    def play_sound(self, name):
+        self.mixer.Sound(self._prefix_path + name + self._suffix_path).play()
+        print("play sound : ", self._prefix_path + name + self._suffix_path)
 
     def play_braille(self, braille):
         if braille[1] == Classification.INITIAL:
@@ -46,6 +46,4 @@ class SoundController:
             print("중성")
         elif braille[1] == Classification.FINAL:
             print("종성")
-
-        self.play_sound(self._prefix_path + braille[0] + self._suffix_path)
-        print("play ", self._prefix_path + braille[0] + self._suffix_path)
+        self.play_sound(braille[0])
